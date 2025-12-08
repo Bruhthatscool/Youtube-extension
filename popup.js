a231dcebc054f3aaf6d4377ff1845e6c3064aac0
Anna <h0p3ful15@gmail.com>
2025-12-09 00:19:35 +0530

    updated manifest n popup.js - initial - save modes to chrome.storage

--- popup.js
+++ popup.js
@@ -1,1 +1,20 @@
 console.log("oru popup aan ith if u didnt understand yet!");
+
+// get checkboxes in popup
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+// when user clicks save button
+document.getElementById("saveBtn").addEventListener("click", () => {
+  const selected = [];
+
+  checkboxes.forEach((i) => {
+    if (i.checked) {
+      selected.push(i.value);
+    }
+  });
+
+  // save to local storage
+  chrome.storage.local.set({ modes: selected }, () => {
+    console.log("saved modes : ", selected);
+  });
+});
