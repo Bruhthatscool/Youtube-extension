446fa0d76404a72ce383a801bfc5692935c2d9ee
Anna <h0p3ful15@gmail.com>
2025-12-10 10:36:46 +0530

    get selected modes from chrome.storage

--- content.js
+++ content.js
@@ -1,3 +1,11 @@
-document.getElementById("button").addEventListener("click", async () => {
-  document.body.style.background = "yellow";
+console.log("content.js loaded yay");
+
+chrome.storage.local.get("modes", (data) => {
+  console.log("modes loaded : ", data.modes);
+
+  // If no modes selected yet, just stop
+  if (!data.modes || data.modes.length === 0) {
+    console.log("No modes selected.");
+    return;
+  }
 });
